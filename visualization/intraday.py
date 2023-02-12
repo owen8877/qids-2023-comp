f9c0a1a8e88d29052a72ca0436488a797184f255
specfazhou <86537418+specfazhou@users.noreply.github.com>
2023-02-11 21:32:54 -0600

    the price_volume function added

--- visualization/intraday.py
+++ visualization/intraday.py
@@ -1,5 +1,4 @@
 from pandas import DataFrame
-from datetime import datetime
 import mplfinance as mpl
 import matplotlib.pyplot as plt
 
@@ -7,10 +6,10 @@ import matplotlib.pyplot as plt
 def plot_price_volume(df: DataFrame, asset: int, day: int):
     new_df = df.loc[(df.asset == asset) & (df.day == day)]
     new_df = new_df[['open', 'high', 'low', 'close', 'volume']]
-    new_df['datetime'] = new_df.index.map(lambda x: datetime.fromtimestamp(x))
+    new_df['datetime'] =  pd.date_range(start='9:30', end='16:00', periods=len(new_df))
     new_df = new_df.set_index('datetime')
     plt.figure(figsize=(20, 10))
-    mpl.plot(new_df, type='candle', volume=True, mav=(5, 10, 20))
+    mpl.plot(new_df, type='candle', volume=True)
     plt.show()
     return None
 

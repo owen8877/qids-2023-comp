d9f47f69c004a1817341360e53a9711c1c6a3f43
owen8877 <9379520+owen8877@users.noreply.github.com>
2023-02-26 23:04:01 -0600

    Value investment does not work.

--- playground/disaspeare/temp.py
+++
@@ -1,8 +0,0 @@
-from xarray import Dataset, DataArray
-
-X: Dataset
-X_transformed: Dataset = wrapped_estimator.fit_transform(X[self.feature])
-
-xr.concat
-
-# https://phausamann.github.io/sklearn-xarray/content/wrappers.html
